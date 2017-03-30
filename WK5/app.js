bd211b915aeb339aa9433a35b5ce119d9557bb49
Shin9626 <akiyachiumi@gmail.com>
2017-03-30 19:29:55 +0800

    HW for WK5
    0330

---
+++ WK5/app.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+
+    if(req.url==='/index.html'){
+        fs.createReadStream(__dirname+'/index.html').pipe(res);       
+    }
+    else if (req.url === '/about.html') {
+        fs.createReadStream(__dirname+'/about.html').pipe(res);
+    }
+
+    else if (req.url === '/') {
+        fs.createReadStream(__dirname+'/index.html').pipe(res);
+    }
+ 
+    else{      
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        var  oops404= fs.readFileSync(__dirname+'/http404.html','utf8');
+        res.end(oops404);
+    }
+    
+}).listen(1234, '127.0.0.1');
